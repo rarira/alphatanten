6757cfb564ae2c770bb916ee8f771f8322689dde
Inseong park <ispark@dinosband.com>
2021-02-22 17:56:29 +0900

    updated..

--- contentscript.js
+++ contentscript.js
@@ -1,7 +1,9 @@
 var s = document.createElement('script');
+
 s.src = chrome.extension.getURL('alphatanten.js');
+console.log(s);
 s.charset = 'UTF-8';
 (document.head || document.documentElement).appendChild(s);
-s.onload = function() {
+s.onload = function () {
   s.parentNode.removeChild(s);
 };
